5b2b7a2f1744e20e98db06ba17df1f51850cf658
marco <marco.spallone@student.unibz.it>
2024-05-14 20:37:22 +0200

    cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Deploy riuscito!&nbsp;
+          Deploy riusciato!&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
