dbba5793fb2189bbcb93267c544895526ea59dcf
marco <marco.spallone@student.unibz.it>
2024-05-17 00:56:13 +0200

    routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/register',
-          destination: '/src/app/pages/register',
-        },
-      ];
-    },
+    
   };
   
   export default nextConfig;
