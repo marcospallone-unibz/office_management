38bead3e95f6fff50eeec0adab2c891c599198c0
marco <marco.spallone@student.unibz.it>
2024-05-20 23:34:02 +0200

    fe

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react";
 import { getOffices } from "../utils/getAPIData";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 
 
 const Dashboard = (data: any) => {
+
+    const searchParams = useSearchParams()
+
+    const [offices, setOffices] = useState([]);
+
+    const getData = async () => {
+        const results = await getOffices(searchParams.get('t'))
+        console.log(results)
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
 
     return (
         <div>
--- src/app/om/dashboard/page.tsx
+++ src/app/om/dashboard/page.tsx
@@ -1,22 +1,14 @@
 "use client";
 
 import Dashboard from "@/app/components/Dashboard";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function CompanyHome() {
 
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-    const [token, setToken] = useState('')
-
-    useEffect(() => {
-        console.log(pathname, searchParams)
-    }, [pathname, searchParams])
-
   return (
     <>
-      <Dashboard data={null}></Dashboard>
+    <Suspense><Dashboard></Dashboard></Suspense>
+
     </>
   );
 }
