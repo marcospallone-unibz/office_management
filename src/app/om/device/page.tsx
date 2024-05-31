762e9b8f592b2eae2275c19ebd379f5782cb4d6c
marco <marco.spallone@student.unibz.it>
2024-05-31 12:44:33 +0200

    fix fe

--- src/app/om/device/page.tsx
+++ src/app/om/device/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import DeviceDashboard from "@/app/components/DeviceDashboard";
+import { Suspense } from "react";
 export default function Office() {
 
   return (
-    <>
+    <Suspense>
       <DeviceDashboard></DeviceDashboard>
-    </>
+    </Suspense>
   );
 }
